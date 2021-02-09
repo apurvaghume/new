ea892cba5d99ddeb188c8da34f4c675dc1142465
Apurva <apurvaghume9@gmail.com>
2021-02-09 18:38:32 +0530

    updated sketch file

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1, p;
 var backgroundImg;
 
 function preload() {
@@ -17,6 +17,8 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    p = new Ground(150,height,300,170)
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -57,6 +59,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+ p.display();
     bird.display();
 }
